ae62b893e744b8a6ae70682eee70fb6049179e9e
无相 <1327235001@qq.com>
2022-09-14 19:20:54 +0800

    修改数据传输

---
+++ js/data/data.js
@@ -0,0 +1,50 @@
+
+//分享界面头部视频信息
+var headVidoe=[
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/eb9mCUpDyx8',
+        'HeadImageURL':'https://i.ytimg.com/vi/eb9mCUpDyx8/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLD-50gmBpt0HJDlrcxTY_MJXFot4g',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/Lsj7ga90Z3E',
+        'HeadImageURL':'https://i.ytimg.com/vi/Lsj7ga90Z3E/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDAfa12FaP0ny54LOhDVc18zT2N4A',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/7bMOhNUA1bI?list=PLpPd_BKEUoYhN8CiOoNLTSVh-7U5yjg3n',
+        'HeadImageURL':'https://i.ytimg.com/vi/7bMOhNUA1bI/hqdefault.jpg?sqp=-oaymwEXCNACELwBSFryq4qpAwkIARUAAIhCGAE=&rs=AOn4CLC6q2O4jFpEUlbkBeY_2ggoHp7j6A',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/tnUPngBEnQ8',
+        'HeadImageURL':'https://i.ytimg.com/vi/tnUPngBEnQ8/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdotEVQnoozKZyIPpoH82VY7qFQg',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/V2NZb0WN7SY',
+        'HeadImageURL':'https://i.ytimg.com/vi/V2NZb0WN7SY/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDW6IV0P8CVDxPZCatp-dL2JLQyLw',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/Ar9eIn4z6XE',
+        'HeadImageURL':'https://i.ytimg.com/vi/Ar9eIn4z6XE/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBtTZaG0HVGZO1mZvEM8LFa4XAyPQ',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/YNHw4-DEpzE',
+        'HeadImageURL':'https://i.ytimg.com/vi_webp/YNHw4-DEpzE/maxresdefault.webp',
+        'titlename':'Unity'
+    },
+    {
+        'HeadvideoURL':'https://www.youtube.com/embed/wvK6MNlmCCE',
+        'HeadImageURL':'https://i.ytimg.com/vi_webp/wvK6MNlmCCE/maxresdefault.webp',
+        'titlename':'Unity'
+    },
+];    
+
+//分享界面右侧视频区域1
+
+//分享界面右侧视频区域2
+
+//分享界面底部视频区域
