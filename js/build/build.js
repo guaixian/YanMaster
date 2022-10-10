a0fb802742e82e43b321e8012e860748fbd60d07
无相 <1327235001@qq.com>
2022-10-10 21:19:27 +0800

    新unity项目提交

--- js/build/build.js
+++ js/build/build.js
@@ -1,13 +1,13 @@
 //添加项目
 
 function addProject(ProjectName,Imagename)
-   {
-	//let UrlTo='Unity/stud1/index.html';
-	let UrlTo='Unity/'+ProjectName+'/index.html';
-	//let ImageURL='images/UnityPro/stud1.png'
-	let ImageURL='images/'+Imagename+'/stud1.png';
-	$(".projects ul").prepend(
-      '<li><div class="project"><div class="project-bg"><a target="_blank"  class="plus">Unity</a><a target="_blank" href="'+UrlTo+'" ><img class="projectimage" src="'+ImageURL+'" alt="" /></a></div></div>'
-    );
-	
-   }
+{
+ //let UrlTo='Unity/stud1/index.html';
+ let UrlTo='Unity/'+ProjectName+'/index.html';
+ //let ImageURL='images/UnityPro/stud1.png'
+ let ImageURL='images/UnityPro/'+Imagename+'.png';
+ $(".projects ul li").prepend(
+   '<div class="project"><div class="project-bg"><a target="_blank"  class="plus">Unity</a><a target="_blank" href="'+UrlTo+'" ><img class="projectimage" src="'+ImageURL+'" alt="" /></a></div></div>'
+ );
+ 
+}
